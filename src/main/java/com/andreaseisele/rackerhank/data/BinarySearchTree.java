a16769b0d13b2b754579d436382ca38fe0243604
Andreas Eisele <ae@andreaseisele.com>
2022-05-23 15:44:50 +0200

    basic binary search tree

---
+++ src/main/java/com/andreaseisele/rackerhank/data/BinarySearchTree.java
@@ -0,0 +1,136 @@
+package com.andreaseisele.rackerhank.data;
+
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BinarySearchTree.class);
+
+    private Node<E> root;
+
+    public boolean contains(E value) {
+        return contains(value, root);
+    }
+
+    public void insert(E value) {
+        root = insert(value, root);
+    }
+
+    @SafeVarargs
+    public final void insertAll(E... values) {
+        for (E value : values) {
+            insert(value);
+        }
+    }
+
+    public void delete(E value) {
+        root = delete(value, root);
+    }
+
+    private boolean contains(E value, Node<E> node) {
+        LOG.debug("contains for {} in {}", value, node);
+        if (node == null) {
+            return false;
+        }
+
+        final var comparison = node.compareValue(value);
+        if (comparison == 0) { // found
+            return true;
+        }
+
+        if (comparison < 0) { // search value is greater than node value
+            return contains(value, node.rightChild);
+        }
+
+        // search value is lesser than node value
+        return contains(value, node.leftChild);
+    }
+
+    private Node<E> insert(E value, Node<E> parent) {
+        LOG.debug("insert for {} in {}", value, parent);
+
+        if (parent == null) {
+            return new Node<>(value);
+        }
+
+        final var comparison = parent.compareValue(value);
+        if (comparison == 0) {
+            LOG.warn("duplicate value -> no insert");
+            return parent;
+        }
+
+        if (comparison < 0) {
+            parent.rightChild = insert(value, parent.rightChild);
+            return parent;
+        }
+
+        parent.leftChild = insert(value, parent.leftChild);
+        return parent;
+    }
+
+    private Node<E> delete(E value, Node<E> parent) {
+        LOG.debug("delete for {} in {}", value, parent);
+
+        if (parent == null) {
+            LOG.warn("value {} not found -> no deletion", value);
+            return null;
+        }
+
+        final var comparison = parent.compareValue(value);
+
+        if (comparison < 0) {
+            parent.rightChild = delete(value, parent.rightChild);
+            return parent;
+        }
+
+        if (comparison > 0) {
+            parent.leftChild = delete(value, parent.leftChild);
+            return parent;
+        }
+
+        // value found
+        if (parent.leftChild == null) {
+            return parent.rightChild;
+        } else if (parent.rightChild == null) {
+            return parent.leftChild;
+        } else {
+            parent.rightChild = lift(parent.rightChild, parent);
+            return parent;
+        }
+    }
+
+    private Node<E> lift(Node<E> node, Node<E> nodeToDelete) {
+        LOG.debug("lift for {} and {}", node, nodeToDelete);
+        if (node.leftChild != null) {
+            node.leftChild = lift(node.leftChild, nodeToDelete);
+            return node;
+        }
+
+        nodeToDelete.value = node.value;
+        return node.rightChild;
+    }
+
+    private static class Node<E extends Comparable<E>> {
+        E value;
+        Node<E> leftChild;
+        Node<E> rightChild;
+
+        Node(E value) {
+            Objects.requireNonNull(value, "node value must not be null");
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node[" + value + "]";
+        }
+
+        int compareValue(E value) {
+            return this.value.compareTo(value);
+        }
+
+    }
+
+}
---
+++ src/test/java/com/andreaseisele/rackerhank/data/BinarySearchTreeTest.java
@@ -0,0 +1,52 @@
+package com.andreaseisele.rackerhank.data;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void testInsertAndContains() {
+        final var tree = new BinarySearchTree<Integer>();
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(2);
+
+        assertThat(tree.contains(1)).isTrue();
+        assertThat(tree.contains(5)).isFalse();
+    }
+
+    @Test
+    void testDeleteWithTwoChildren() {
+        final var tree = new BinarySearchTree<Integer>();
+        tree.insertAll(50, 25, 75, 10, 33, 56, 89, 4, 11, 30, 40, 52, 61, 82, 95);
+
+        tree.delete(56);
+
+        assertThat(tree.contains(56)).isFalse();
+        assertThat(tree.contains(52)).isTrue();
+        assertThat(tree.contains(61)).isTrue();
+    }
+
+    @Test
+    void testDeleteLeaf() {
+        final var tree = new BinarySearchTree<Integer>();
+        tree.insertAll(50, 25, 75, 10, 33, 56, 89, 4, 11, 30, 40, 52, 61, 82, 95);
+
+        tree.delete(95);
+
+        assertThat(tree.contains(95)).isFalse();
+    }
+
+    @Test
+    void testDeleteNonExisting() {
+        final var tree = new BinarySearchTree<Integer>();
+        tree.insertAll(3, 2, 1);
+
+        assertDoesNotThrow(() -> tree.delete(4));
+    }
+
+}
